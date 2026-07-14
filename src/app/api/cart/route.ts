9ad524f3a81e40b4e26a58946ec83b70bc3e37b0
maira32 <mairatahir3@gmail.com>
2026-07-14 17:42:43 +0500

    to see cart

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -5,6 +5,8 @@ import { connectDB } from '@/lib/mongodb';
 import Cart from '@/models/Cart';
 import Product from '@/models/Product'; 
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const session = await getServerSession(authOptions);
